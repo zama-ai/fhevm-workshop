29d855c588cf8c83ed343b9cc2d231eb958ca35e
Morten Dahl <mortendahlcs@gmail.com>
2024-05-21 13:55:22 +0200

    feat: switch to euint64

--- erc20.sol
+++ erc20.sol
@@ -13,7 +13,7 @@ contract EncryptedERC20 is EIP712Reencrypt {
         contractOwner = msg.sender;
     }
 
-    mapping(address => euint32) internal balances;
+    mapping(address => euint64) internal balances;
 
     function balanceOf(
         address wallet,
@@ -34,14 +34,30 @@ contract EncryptedERC20 is EIP712Reencrypt {
         balances[contractOwner] = TFHE.add(balances[contractOwner], amount);
     }
 
-        euint32 amount = TFHE.asEuint32(amountCiphertext);
-        TFHE.req(TFHE.le(amount, balances[msg.sender]));
-
-        balances[to] = TFHE.add(balances[to], amount);
-        balances[msg.sender] = TFHE.sub(balances[msg.sender], amount);
     function transfer(address to, bytes calldata amountCiphertext) public returns (bool) {
+        return transfer_leaking(to, amountCiphertext);
+        // return transfer_nonleaking(to, amountCiphertext);
     }
 
+    function transfer_leaking(address to, bytes calldata amountCiphertext) internal returns (bool) {
+        euint64 amount = TFHE.asEuint64(amountCiphertext);
+
+        bool sufficient = TFHE.decrypt(TFHE.le(amount, balances[msg.sender]));
+        if (sufficient) {
+            balances[to] = balances[to] + amount;
+            balances[msg.sender] = balances[msg.sender] - amount;
+        }
+
+        return sufficient;
     }
 
+    function transfer_nonleaking(address to, bytes calldata amountCiphertext) internal returns (bool) {
+        euint64 amount = TFHE.asEuint64(amountCiphertext);
+
+        ebool sufficient = TFHE.le(amount, balances[msg.sender]);
+        balances[to] = TFHE.select(sufficient, balances[to] + amount, balances[to]);
+        balances[msg.sender] = TFHE.select(sufficient, balances[msg.sender] - amount, balances[msg.sender]);
+        
+        return true;
+    }
 }
