015f09ff071c943645f3f339cedc2a52fda03d2a
Morten Dahl <mortendahlcs@gmail.com>
2024-05-21 13:54:43 +0200

    chore: bring up to date

--- erc20.sol
+++ erc20.sol
@@ -1,22 +1,22 @@
 // SPDX-License-Identifier: BSD-3-Clause-Clear
 
-pragma solidity >=0.8.13 <0.8.20;
+pragma solidity ^0.8.20;
 
 import "fhevm/lib/TFHE.sol";
 
-import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
-import "@openzeppelin/contracts/utils/cryptography/EIP712.sol";
+import "./eip712-modifier.sol";
 
-contract EncryptedERC20 is EIP712 {
+contract EncryptedERC20 is EIP712Reencrypt {
     address public contractOwner;
 
-    constructor() EIP712("Authorization token", "1") {
+    constructor() {
         contractOwner = msg.sender;
     }
 
     mapping(address => euint32) internal balances;
 
     function balanceOf(
+        address wallet,
         bytes32 publicKey,
         bytes calldata signature
     )
@@ -25,35 +25,23 @@ contract EncryptedERC20 is EIP712 {
         signedBySender(publicKey, signature)
         returns (bytes memory)
     {
+        require(wallet == msg.sender);
         return TFHE.reencrypt(balances[msg.sender], publicKey, 0);
     }
 
-    function mint(bytes calldata amountCiphertext) public {
+    function mint(uint64 amount) public {
         require(msg.sender == contractOwner);
-        euint32 amount = TFHE.asEuint32(amountCiphertext);
         balances[contractOwner] = TFHE.add(balances[contractOwner], amount);
     }
 
-    function transfer(address to, bytes calldata amountCiphertext) public {
         euint32 amount = TFHE.asEuint32(amountCiphertext);
         TFHE.req(TFHE.le(amount, balances[msg.sender]));
 
         balances[to] = TFHE.add(balances[to], amount);
         balances[msg.sender] = TFHE.sub(balances[msg.sender], amount);
+    function transfer(address to, bytes calldata amountCiphertext) public returns (bool) {
     }
 
-    modifier signedBySender(bytes32 publicKey, bytes memory signature) {
-        bytes32 digest = _hashTypedDataV4(
-            keccak256(
-                abi.encode(keccak256("Reencrypt(bytes32 publicKey)"), publicKey)
-            )
-        );
-        address signer = ECDSA.recover(digest, signature);
-        require(
-            signer == msg.sender,
-            "EIP712 signer and transaction signer do not match"
-        );
-        _;
     }
 
 }
